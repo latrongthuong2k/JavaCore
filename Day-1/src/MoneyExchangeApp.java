040e748a5e24b0077042129693280dbea87ccbbd
LA TRONG THUONG <74745602+latrongthuong2k@users.noreply.github.com>
2023-07-18 00:31:15 +0900

    Update day 1

---
+++ Day-1/src/MoneyExchangeApp.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class MoneyExchangeApp {
+    public static void main(String[] args) {
+        float exchangeRate = 23000;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập số tiền muốn chuyển đổi : ... USD");
+        float usdAmount = scanner.nextFloat();
+
+        float exchangedMoney = usdAmount * exchangeRate;
+        System.out.println("Số tiền VND sau khi chuyển đổi là : ");
+        System.out.printf("%.2f USD = %.2f VND",usdAmount,exchangedMoney );
+     /* phím tắt xuống dòng
+     shift enter
+
+     * */
+    }
+
+}
