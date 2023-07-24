f1283b606298d1bbe64514b53fd9c7330d733e71
LA TRONG THUONG <74745602+latrongthuong2k@users.noreply.github.com>
2023-07-25 00:29:58 +0900

    update

--- Day-3-ss4/src/FindTheSecondHighestE.java
+++ Day-3-ss4/src/FindTheSecondHighestE.java
@@ -2,7 +2,7 @@ public class FindTheSecondHighestE {
 
     public static void main(String[] args) {
         int[] arr = {5, 3, 9, 1, 7, 6, 8};
-        // tránh trường hợp trong mảng là số âm thì khi cho bằng 0 thì sẽ không check được giá trị âm 
+        // tránh trường hợp trong mảng là số âm thì khi cho bằng 0 thì sẽ không check được giá trị âm
         int largest = Integer.MIN_VALUE;
         int secondLargest = Integer.MIN_VALUE;
 
